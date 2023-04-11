57d833d700e85623b0af78a6db670b99af632ce1
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-04-11 02:24:54 +0200

    Changes to be committed: 	new file:   .expo/README.md 	new file:   .expo/devices.json 	new file:   .expo/packager-info.json 	new file:   .expo/settings.json 	new file:   .gitignore 	new file:   App.js 	new file:   Components/Button.js 	new file:   Screens/GameOverScreen.js 	new file:   Screens/GameScreen.js 	new file:   Screens/StartGameScreen.js 	new file:   app.json 	new file:   assets/adaptive-icon.png 	new file:   assets/favicon.png 	new file:   assets/icon.png 	new file:   assets/splash.png 	new file:   babel.config.js 	new file:   package-lock.json 	new file:   package.json     # First Comit XD

---
+++ Screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View , StyleSheet , Text , TextInput , Button } from 'react-native'
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput placeholder='Add' />
+     <Text> StartGameScreen</Text>
+    </View>
+  )
+}
+
+export default StartGameScreen
