2286ffea5a01075218a9188c93a19a4b55f187f6
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-04-12 17:10:54 +0200

    Changes to be committed:     modified:   App.js 	deleted:    Components/Button.js     new file:   Components/PrimartButton.js 	modified:   Screens/StartGameScreen.js

--- App.js
+++ App.js
@@ -4,18 +4,9 @@ import StartGameScreen from './Screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <StartGameScreen /> 
-      <StatusBar style="auto" />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -1,13 +1,49 @@
-import React from 'react'
-import { View , StyleSheet , Text , TextInput , Button } from 'react-native'
+import React from "react";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import PrimaryButton from "../Components/PrimartButton";
+import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput placeholder='Add' />
-     <Text> StartGameScreen</Text>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        placeholder="Add"
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
-  )
-}
+  );
+};
 
-export default StartGameScreen
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 12,
+    shadowColor: "red",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    borderBottomColor: "#ddb52f",
+    fontSize: 32,
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
+  },
+});
